0e392334e46063d5f5843dd3a246a2735733106f
kumarrahul771996 <kumarrahul771996@gmail.com>
2024-02-21 23:06:56 +0000

    chore(feat): customer login

---
+++ lib/customer/customer_login.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/customer/customer_dashboard.dart';
+import 'package:myapp/customer/customer_register.dart';
+
+class CustomerLogin extends StatefulWidget {
+  const CustomerLogin({super.key});
+
+  @override
+  State<CustomerLogin> createState() => _CustomerLoginState();
+}
+
+class _CustomerLoginState extends State<CustomerLogin> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitform() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(_formKey.currentContext!).showSnackBar(
+        const SnackBar(
+          content: Text("Login successful"),
+        ),
+      );
+      // TODO: implement flush here
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return const CustomerDashboard();
+          },
+        ),
+      );
+    }
+  }
+
+  String? _validateEmail(value) {
+    if (value!.isEmpty) {
+      return 'Please enter email';
+    }
+    RegExp emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _validatePassword(value) {
+    if (value!.isEmpty) {
+      return 'Please enter password';
+    }
+    if (value.length != 8) {
+      return 'Please enter password with minimum 8 characters';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Text(
+                "Welcome, Login to continue",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: _validateEmail,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                validator: _validatePassword,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+              ),
+              const SizedBox(height: 20),
+              // TODO: Add remember me and forgot password?
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Remember me"),
+                  Text(
+                    "Forgot password?",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _submitform,
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  const SizedBox(width: 10),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const CustomerRegister();
+                          },
+                        ),
+                      );
+                    },
+                    child: const Text("Sign Up"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text("OR"),
+              const SizedBox(height: 10),
+              const ListTile(
+                // TODO: change logo
+                leading: Icon(Icons.account_balance),
+                title: Text("Sign In with Google"),
+              ),
+              const ListTile(
+                leading: Icon(Icons.sms),
+                title: Text("Sign In with OTP"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/serviceProvider/service_provider_login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Testsp extends StatelessWidget {
+  const Testsp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text("Service Provider login page"),
+      ),
+    );
+  }
+}
