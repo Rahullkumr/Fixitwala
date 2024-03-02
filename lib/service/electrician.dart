0e811033b6fdb7f496b31bbd03cf0836bdf515d1
kumarrahul771996 <kumarrahul771996@gmail.com>
2024-03-02 00:45:33 +0000

    feat: add landing page; use gridview.builder()

---
+++ lib/service/electrician.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ElectricianPage extends StatelessWidget {
+  const ElectricianPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Electrician Page")),
+    );
+  }
+}
